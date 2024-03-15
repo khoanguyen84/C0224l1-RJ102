8ccef7b0fc41d3c64bec9eed30b94023f5aaf396
KhoaNguyen <nguyenhuuanhkhoa@gmail.com>
2024-03-15 20:16:14 +0700

    XMLHTTPRequest

--- staff-app/staff.js
+++ staff-app/staff.js
@@ -28,9 +28,12 @@ function renderStaffList() {
                             </li>
                         </ul>
                         <div class="card-footer">
-                            <button class="btn btn-danger"
-                                onclick='removeStaff(${staff.id})'
+                            <button class="btn btn-danger btn-sm"
+                                onclick='removeStaff("${staff.id}")'
                             >Delete</button>
+                            <button class='btn btn-warning btn-sm'
+                                onclick='getStaff(${JSON.stringify(staff)})'
+                            >Modify</buton>
                         </div>
                     </div>
                 </div>
@@ -91,6 +94,63 @@ function cleanForm() {
     document.getElementById('avatarUrl').value = null
     document.getElementById('dob').value = null
     document.getElementById('gender').value = true
+    document.getElementById('staffId').value = 0
+}
+
+
+function getStaff(staff){
+    document.getElementById('btn-create').classList.add('d-none')
+    document.getElementById('btn-update').classList.remove('d-none')
+    document.getElementById('btn-cancel').classList.remove('d-none')
+    document.getElementById('form-title').innerHTML = 'Update Staff'
+
+
+    document.getElementById('fullname').value = staff.fullname
+    document.getElementById('email').value = staff.email
+    document.getElementById('dob').value = dayjs(staff.dob).format('YYYY-MM-DD')
+    document.getElementById('avatarUrl').value = staff.avatarUrl
+    document.getElementById('gender').value = staff.gender
+    document.getElementById('mobile').value = staff.mobile
+    document.getElementById('staffId').value = staff.id
+}
+
+function resetCreateStaffForm(){
+    document.getElementById('btn-create').classList.remove('d-none')
+    document.getElementById('btn-update').classList.add('d-none')
+    document.getElementById('btn-cancel').classList.add('d-none')
+    document.getElementById('form-title').innerHTML = 'Create Staff'
+
+    cleanForm()
+}
+
+function updateStaff(){
+    let fullname = document.getElementById('fullname').value
+    let email = document.getElementById('email').value
+    let mobile = document.getElementById('mobile').value
+    let avatarUrl = document.getElementById('avatarUrl').value
+    let dob = document.getElementById('dob').value
+    let gender = document.getElementById('gender').value
+    let staffId = Number(document.getElementById('staffId').value)
+    let staff = {
+        fullname: fullname,
+        email: email,
+        mobile: mobile,
+        avatarUrl: avatarUrl,
+        dob: Date(dob),
+        gender: Boolean(gender)
+    }
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("PUT", `https://jsonserver-vercel-api.vercel.app/staffs/${staffId}`)
+    xhr.setRequestHeader('Content-Type', "application/json")
+    xhr.send(JSON.stringify(staff))
+    xhr.onload = function(){
+        if(xhr.status === 200 && xhr.readyState === 4) {
+            alert('Staff update success')
+            renderStaffList()
+            resetCreateStaffForm();
+        }
+    }
 }
 renderStaffList()
 
