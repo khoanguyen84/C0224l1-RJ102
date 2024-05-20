fb0cf97bea5c96f769a46d3bc84c0459ca38ab12
KhoaNguyen <nguyenhuuanhkhoa@gmail.com>
2024-05-20 21:38:08 +0700

    use client

---
+++ next-js/src/app/(account)/login/login-button.js
@@ -0,0 +1,6 @@
+'use client'
+export default function LoginButton(){
+    return (
+        <button onClick={() => alert("Login")}>Login</button>
+    )
+}
--- next-js/src/app/(account)/login/page.js
+++ next-js/src/app/(account)/login/page.js
@@ -1,5 +1,10 @@
+import LoginButton from "./login-button";
+
 export default function LoginPage(){
     return (
-        <h1>Login Page</h1>
+        <>
+            <h1>Login Page</h1>
+            <LoginButton/>
+        </>
     )
 }
